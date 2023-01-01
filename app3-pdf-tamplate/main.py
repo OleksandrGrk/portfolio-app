da7c2b42128a8821cedad0a6b45a820bdd1bf1d4
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-01-01 20:41:56 +0200

    add more pages for each topic

--- app3-pdf-tamplate/main.py
+++ app3-pdf-tamplate/main.py
@@ -9,10 +9,14 @@ for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 50, 50)
-
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
 
     pdf.line(10, 22, 200, 22)
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+
+
 
 
 pdf.output("output.pdf")
